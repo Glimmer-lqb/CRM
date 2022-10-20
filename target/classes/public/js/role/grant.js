4dd41b79df1a78554686a86f7033740c680ead12
Glimmer-lqb <lqb200004@163.com>
2022-10-20 13:30:54 +0800

    10.20第一次提交

---
+++ target/classes/public/js/role/grant.js
@@ -0,0 +1,45 @@
+$(function () {
+    // 加载树形结构
+    loadModuleData();
+});
+
+// 定义树形结构对象
+var zTreeObj;
+
+
+/**
+ * 加载资源树形数据
+ */
+function loadModuleData() {
+    // 配置信息对象  zTree的参数配置
+    var setting = {
+        // 使用复选框
+        check:{
+            enable:true
+        },
+        // 使用简单的JSON数据
+        data:{
+            simpleData:{
+                enable: true
+            }
+        }
+    }
+
+
+    // 数据
+    // 通过ajax查询资源列表
+    $.ajax({
+        type:"get",
+        url:ctx + "/module/queryAllModules",
+        // 查询所有的资源列表时，传递角色ID，查询当前角色对应的已经授权的资源
+        data:{
+            roleId:$("[name='roleId']").val()
+        },
+        dataType:"json",
+        success:function (data) {
+            // data:查询到的资源列表
+            // 加载zTree树插件
+            zTreeObj = $.fn.zTree.init($("#test1"), setting, data);
+        }
+    });
+}
